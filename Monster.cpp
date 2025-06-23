39188f5f10600cf47a9339f8546faee39cfcb92a
코모 <sjm7829@naver.com>
2025-06-23 20:31:58 +0900

    3차 수정본 monster.cpp만 / bool 변경 및 두 함수 합체

--- Monster.cpp
+++ Monster.cpp
@@ -14,23 +14,22 @@ int Student1::getHealth()
 {
 	return health;
 }
-void Student1::takeDamage(int damage)
+
+bool Student1::takeDamage(int damage)
 {
 	health -= damage;
 
 	if (health < 0)
 	{
 		health = 0;
-	}
-
-	cout << "남은 체력 : " << health << endl;
-	cout << "으악! 과제 그만!" << endl;
-}
-void Student1::checkDead()
-{
-	if (health <= 0)
-	{
 		cout << name << "이 과제에 쌓여 쓰러졌습니다." << endl;
+		return true;
+	}
+	else
+	{
+		cout << "남은 체력 : " << health << endl;
+		cout << "으악! 과제 그만!" << endl;
+		return false;
 	}
 }
 
@@ -47,26 +46,24 @@ int Student2::getHealth()
 {
 	return health;
 }
-void Student2::takeDamage(int damage)
+
+bool Student2::takeDamage(int damage)
 {
 	health -= damage;
 
 	if (health < 0)
 	{
 		health = 0;
-	}
-
-	cout << "남은 체력 : " << health << endl;
-	cout << "흑흑 과제를 또 주시는건가요?" << endl;
-}
-void Student2::checkDead()
-{
-	if (health <= 0)
-	{
 		cout << name << "이 과제에 쌓여 쓰러졌습니다." << endl;
+		return true;
+	}
+	else
+	{
+		cout << "남은 체력 : " << health << endl;
+		cout << "흑흑 과제를 또 주시는건가요?" << endl;
+		return false;
 	}
 }
-
 
 //--- Student3
 string Student3::getName()
@@ -81,26 +78,23 @@ int Student3::getHealth()
 {
 	return health;
 }
-void Student3::takeDamage(int damage)
+bool Student3::takeDamage(int damage)
 {
 	health -= damage;
 
 	if (health < 0)
 	{
 		health = 0;
-	}
-
-	cout << "남은 체력 : " << health << endl;
-	cout << "하하.. 하하.. 과제다.." << endl;
-}
-void Student3::checkDead()
-{
-	if (health <= 0)
-	{
 		cout << name << "이 과제에 쌓여 쓰러졌습니다." << endl;
+		return true;
+	}
+	else
+	{
+		cout << "남은 체력 : " << health << endl;
+		cout << "하하.. 하하.. 과제다.." << endl;
+		return false;
 	}
 }
-
 
 //--- Student4::
 string Student4::getName()
@@ -115,26 +109,23 @@ int Student4::getHealth()
 {
 	return health;
 }
-void Student4::takeDamage(int damage)
+bool Student4::takeDamage(int damage)
 {
 	health -= damage;
 
 	if (health < 0)
 	{
 		health = 0;
-	}
-
-	cout << "남은 체력 : " << health << endl;
-	cout << "과제.. 해야죠.. 그럼요.." << endl;
-}
-void Student4::checkDead()
-{
-	if (health <= 0)
-	{
 		cout << name << "이 과제에 쌓여 쓰러졌습니다." << endl;
+		return true;
+	}
+	else
+	{
+		cout << "남은 체력 : " << health << endl;
+		cout << "과제.. 해야죠.. 그럼요.." << endl;
+		return false;
 	}
 }
-
 
 //--- Student5
 string Student5::getName()
@@ -149,26 +140,23 @@ int Student5::getHealth()
 {
 	return health;
 }
-void Student5::takeDamage(int damage)
+bool Student5::takeDamage(int damage)
 {
 	health -= damage;
 
 	if (health < 0)
 	{
 		health = 0;
-	}
-
-	cout << "남은 체력 : " << health << endl;
-	cout << "어.. 잘 모르겠지만 해보겠습니다.." << endl;
-}
-void Student5::checkDead()
-{
-	if (health <= 0)
-	{
 		cout << name << "이 과제에 쌓여 쓰러졌습니다." << endl;
+		return true;
+	}
+	else
+	{
+		cout << "남은 체력 : " << health << endl;
+		cout << "어.. 잘 모르겠지만 해보겠습니다.." << endl;
+		return false;
 	}
 }
-
 
 //--- Student6
 string Student6::getName()
@@ -183,22 +171,20 @@ int Student6::getHealth()
 {
 	return health;
 }
-void Student6::takeDamage(int damage)
+bool Student6::takeDamage(int damage)
 {
 	health -= damage;
 
 	if (health < 0)
 	{
 		health = 0;
-	}
-
-	cout << "남은 체력 : " << health << endl;
-	cout << "다 했습니다! 과제 더 주실건가요?" << endl;
-}
-void Student6::checkDead()
-{
-	if (health <= 0)
-	{
 		cout << name << "이 과제에 쌓여 쓰러졌습니다." << endl;
+		return true;
+	}
+	else
+	{
+		cout << "남은 체력 : " << health << endl;
+		cout << "다 했습니다! 과제 더 주실건가요?" << endl;
+		return false;
 	}
 }
